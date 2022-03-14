8a3f0fe4cc5be235c1bd7906029677c5a130e1d0
Sebastian Frey <sebastian.frey@outlook.com>
2022-03-14 20:09:15 +0100

    Update demo

--- index.mjs
+++ index.mjs
@@ -1,4 +1,4 @@
-import esriConfig from 'https://js.arcgis.com/next/@arcgis/core/config.js';
+import esriConfig from "https://js.arcgis.com/next/@arcgis/core/config.js";
 import Map from "https://js.arcgis.com/next/@arcgis/core/Map.js";
 import MapView from "https://js.arcgis.com/next/@arcgis/core/views/MapView.js";
 import WMSLayer from "https://js.arcgis.com/next/@arcgis/core/layers/WMSLayer.js";
@@ -6,46 +6,45 @@ import LayerList from "https://js.arcgis.com/next/@arcgis/core/widgets/LayerList
 
 esriConfig.request.maxUrlLength = 400;
 
-const bootstrap = () => {
-  const map = new Map({
-    basemap: {
-      baseLayers: [],
-    },
-    layers: [new WMSLayer({
-      url: "https://sgx.geodatenzentrum.de/wms_topplus_open",
-    })],
-  });
+const map = new Map({
+  basemap: {
+    baseLayers: [],
+  },
+  layers: [],
+});
 
-  const spatialReference = {
-    wkid: 25832,
-  };
+const view = new MapView({
+  container: "map",
+  map: map,
+});
 
-  const extent = {
-    spatialReference,
-    xmin: 530031.723903733,
-    ymin: 5892333.447497229,
-    xmax: 561004.9900602706,
-    ymax: 5898786.21127984,
-  };
+let layerList = new LayerList({
+  view: view,
+});
+// Adds widget below other elements in the top left corner of the view
+view.ui.add(layerList, {
+  position: "top-left",
+});
 
-  const view = new MapView({
-    container: 'map',
-    map: map,
-    spatialReference,
-    extent,
-  });
+view.ui.add(document.getElementById("description"), {
+  position: "top-right",
+});
 
-  let layerList = new LayerList({
-    view: view,
-  });
-  // Adds widget below other elements in the top left corner of the view
-  view.ui.add(layerList, {
-    position: "top-left"
-  });
+function loadLayer() {
+  const formData = Object.fromEntries(
+    new FormData(document.getElementById("layerForm"))
+  );
 
-  view.ui.add(document.getElementById('description'), {
-    position: "top-right"
-  });
-};
+  map.layers.removeAll();
+  map.layers.add(
+    new WMSLayer({
+      url: formData.url,
+    })
+  );
+}
 
-window.onload = bootstrap;
+loadLayer();
+
+document
+  .getElementById("loadLayerButton")
+  .addEventListener("click", () => loadLayer());
